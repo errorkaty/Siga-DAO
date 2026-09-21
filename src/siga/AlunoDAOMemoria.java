bd4c1b39f8cd1de45ac3f0bf3dcb05b6e7791cfa
Katllyn Rezende <Katllynrezende@gmail.com>
2026-09-21 15:41:25 -0300

    Criação de "AlunoDAOMemoria"

---
+++ src/siga/AlunoDAOMemoria.java
@@ -0,0 +1,36 @@
+package siga;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AlunoDAOMemoria implements AlunoDAO{
+    
+    private final Map<String, Aluno> alunos = new LinkedHashMap<>();
+
+    @Override
+    public void inserir(Aluno aluno) {
+        alunos.put(aluno.getMatricula(), aluno);
+    }
+
+    @Override 
+    public Aluno buscarPorMatricula(String matricula) {
+        return alunos.get(matricula);
+    }
+
+    @Override
+    public List<Aluno> buscarTodos() {
+        return new ArrayList<>(alunos.values());
+    }
+
+    @Override
+    public void atualizar(Aluno aluno) {
+        alunos.put(aluno.getMatricula(), aluno);
+    }
+
+    @Override
+    public void remover(String matricula) {
+        alunos.remove(matricula);
+    }
+}
